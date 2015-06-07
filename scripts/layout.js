9d5f3ac4b9e9d2a6ceffcbc1045d687399b977b0
Spike Mak <msh.spike@gmail.com>
2015-06-08 02:36:09 +0800

    Now it looks way better... Still need Hex to RGB conversion though

---
+++ scripts/layout.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+    $("#center-box").css("left",($(document).width())/2-200);
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,96 @@
-console.log($(".rgb-input").length);
+/**
+ * Document ready function. Set a random color when page is reloaded
+ */
+$(document).ready(function() {
+    setColor((Math.floor(Math.random()*255)),(Math.floor(Math.random()*255)),(Math.floor(Math.random()*255)));
+});
+
+/**
+* Capture "tab" key on the 3 RGB input.
+* It is to allow using tab key to loop through each RGB color.
+*/
+$(".block-content").on("keydown", "[id^=text]", function(e) {
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == 9) {
+        e.preventDefault();
+
+        var text_input_arr = $("[id^=text]");
+        for (var i=0; i<text_input_arr.length; i++) {
+            if (text_input_arr[i] == $(this)[0]) {
+                if (i == 2) { $("[id^=text]")[0].focus(); $("[id^=text]")[0].select(); }
+                else { $("[id^=text]")[i+1].focus(); $("[id^=text]")[i+1].select(); }
+                return 1;
+            }
+        }
+    }
+});
+
+/**
+ * Onchange listener on the 3 RGB color input.
+ */
 $(".rgb-input").change(function() {
     $(this).val(verifyColor($(this).val()));
 
     var type = $(this).attr("type");
-    var rgb = [
-                verifyColor($("#"+type+"_red").val()),
+    var rgb  = [verifyColor($("#"+type+"_red").val()),
                 verifyColor($("#"+type+"_green").val()),
-                verifyColor($("#"+type+"_blue").val())
-            ];
-    if (type == "number") {
+                verifyColor($("#"+type+"_blue").val()) ];
+
+    if (type == "text") {
         $("[id^=range]").each(function(i) { $(this).val(rgb[i]); });
     } else {
-        $("[id^=number]").each(function(i) { $(this).val(rgb[i]); });
+        $("[id^=text]").each(function(i) { $(this).val(rgb[i]); });
     }
-    $("#number_red").css("color", "#"+colortohex(rgb[0])+"0000");
-    $("#number_green").css("color", "#00"+colortohex(rgb[1])+"00");
-    $("#number_blue").css("color", "#0000"+colortohex(rgb[2]));
-    $("body").css("background-color", "#"+rgbtohex(rgb));
+    $("#hex").val("#"+rgbtohex(rgb[0], rgb[1], rgb[2]));
+
+    setColor(rgb[0],rgb[1],rgb[2]);
 });
+
+$("[id*=text_]").on("paste", function(e) {
+    var pastedText;
+/*
+    if (window.clipboardData && window.clipboardData.getData) { // IE
+        pastedText = window.clipboardData.getData('Text');
+    } else if (e.clipboardData && e.clipboardData.getData) {
+        pastedText = e.clipboardData.getData('text/plain');
+    }
+    console.log("pastedText = " + pastedText);
+*/
+});
+
+function setEach(color, value) {
+    var rgb = "rgb(";
+    if (color == "red") { rgb += value + ",0,0)"; }
+    else if (color == "green") { rgb += "0," + value + ",0"; }
+    else if (color == "blue") { rgb += "0,0," + value; }
+    else { console.log("Error setting color. Passed color = " + color); return false; }
+
+    $("#text_"+color).css("color", rgb);
+    $("#range_"+color).val(value);
+    $("#text_"+color).val(value);
+}
+
+function setColor(r,g,b) {
+    // Color block text
+    setEach("red",   r);
+    setEach("green", g);
+    setEach("blue",  b);
+
+    // Hex & Background
+    $("#hex").val("#"+rgbtohex(r,g,b));
+    $("body").css("background-color","rgb("+r+","+g+","+b+")")
+}
 
 function hextorgb(hex) {
     var rgb = [];
     return rgb;
+}
+
+function rgbtohex(r,g,b) {
+    var rgb = [r,g,b];
+    var hex = [];
+    $.each(rgb, function(index, value) { hex.push(colortohex(value)); });
+    return (hex[0] + hex[1] + hex[2]).toUpperCase();
 }
 
 function colortohex(color) {
@@ -30,24 +99,21 @@ function colortohex(color) {
     return hex;
 }
 
-function rgbtohex(rgb) {
-    var hex = [];
-    $.each(rgb, function(index, value) {
-        hex.push(colortohex(value));
-    });
-    return (hex[0] + hex[1] + hex[2]);
-}
-
+/**
+ * Verify the data from the 3 RGB input.
+ * The raw input will be parsed into integer.
+ * If out of bound, value is set to the closest bound (min/max), if it's invalid string, set it to 0.
+ * 
+ * @param  {String} col Raw input from the textfield
+ * @return {Integer}     Parsed integer which will be assigned back to the textfield.
+ */
 function verifyColor(col) {
+    var parsed = parseInt(col, 10);
     var corr = 0;
-
-    if ((col == "") || (col < 0)) {
-        corr = 0;
-    } else if (col > 255) {
-        corr = 255;
-    } else {
-        corr = col;
+    if (!isNaN(parsed)) {
+        if (parsed > 255) { corr = 255; }
+        else if (parsed < 0) { corr = 0; }
+        else { corr = parsed; }
     }
-
-    return parseInt(corr, 10);
+    return corr;
 }
